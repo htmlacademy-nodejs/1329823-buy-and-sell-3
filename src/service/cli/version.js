e302cae30e74ad197f42b668026251cd1c64a46f
Liz_CHe <alishia@list.ru>
2020-01-28 15:58:50 +0300

    Почти сделала, осталось протеститоровать

---
+++ src/service/cli/version.js
@@ -0,0 +1,12 @@
+;
+'use strict';
+const packageJsonFile = require(`../../../package.json`);
+module.export = {
+    name: `--version`,
+    run(){
+        const version = packageJsonFile.version;
+        console.info(version);
+    }
+
+}
+
--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,22 @@
+;
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const {
+    DEFAULT_COMMAND,
+    USER_ARGV_INDEX,
+    ExitCode
+} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand,userItemCount] = userArguments;
+
+if (userArguments.length ===0 || !Cli[userCommand]){
+    Cli[DEFAULT_COMMAND].run();
+    process.exit(ExitCode.success);
+}
+if (userItemCount>1000){
+    console.info(`Не больше 1000 объявлений`);
+    process.exit(ExitCode.error);
+}
+Cli[userCommand].run(userItemCount);
