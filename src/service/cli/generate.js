895b627b7e49bd8b05a4fda123897c39bdea46cd
Liz_CHe <alishia@list.ru>
2020-02-13 13:30:38 +0300

    async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,7 +1,7 @@
 'use strict'; 
 
 const {ExitCode} = require(`../../constants`); 
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {generateOffers, makeMock} = require(`../cli/utils`); 
 const DEFAULT_COUNT = 1; 
@@ -10,7 +10,7 @@ const FILE_NAME = `mocks.json`;
 
 module.exports = { 
     name: `--generate`, 
-    run(userIndex) { 
+    async run(userIndex) { 
         const [count] = userIndex; 
         if (count > MAX_COUNT) { 
             console.error(chalk.red(`Не больше ${MAX_COUNT} объявлений`)); 
@@ -18,13 +18,14 @@ module.exports = {
         } 
         const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT; 
         const content = JSON.stringify(generateOffers(countOffer));
-        fs.writeFileSync(FILE_NAME,content,(err) =>{
-            if (err){ 
-                console.error(chalk.red(`Не удалось записать данные в файл`)); 
-                process.exit(ExitCode.error);
-            }
-            console.info(chalk.green(`Файл записан!`)); 
+        try{
+            await fs.writeFile(FILE_NAME, content);
+            console.log(chalk.green(`File recorded!`)); 
             process.exit(ExitCode.success);
-        });
+        }
+        catch {
+            console.error(chalk.red(`Error writing file`)); 
+            process.exit(ExitCode.error);
+        }
     } 
 };
