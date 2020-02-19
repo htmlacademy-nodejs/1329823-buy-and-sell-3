4e99d4d0343d03fd1a986e8540aa6ad98c142319
Liz_CHe <alishia@list.ru>
2020-02-19 17:08:37 +0300

    Data from files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,23 +1,58 @@
 'use strict'; 
 
-const {ExitCode} = require(`../../constants`); 
+const {ExitCode} = require(`../../constants`);  
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-const {generateOffers, makeMock} = require(`../cli/utils`); 
+const {getReandomInt,shuffle} = require(`../cli/utils`); 
 const DEFAULT_COUNT = 1; 
 const MAX_COUNT = 1000;
 const FILE_NAME = `mocks.json`;
 
+const OfferType = {offer: `offer`, sale: `sale`,}; 
+const SumRestrict = {min: 1000, max: 100000,}; 
+const PictureRestrict = {min: 0, max: 16,}; 
+const getPicFileName = (number) => `item${number<10?`0${number}`:number}.jpg}`;
+
+const generateOffers = (count,titles,categories,sentences) => (
+    Array(count).fill({}).map( () => (
+        {
+            title: titles[getReandomInt(0,titles.length-1)],
+            picture: getPicFileName(getReandomInt(PictureRestrict.min,PictureRestrict.max)),
+            description: shuffle(sentences).slice(1,5).join(` `),
+            type: Object.keys(OfferType)[Math.floor(Math.random()*Object.keys(OfferType).length)],
+            sum: getReandomInt(SumRestrict.min,SumRestrict.max),
+            category: [categories[getReandomInt(0,categories.length-1)]],
+        }))
+);
+//пути к текстовым файлам
+const FILE_SENTENCES_PATH = `../../data/sentences.txt`;
+const FILE_TITLES_PATH = `../../data/titles.txt`;
+const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
+
+const readContent = async (filePath) => {
+    try {
+        const content = await fs.readFile(filePath, `utf8`);
+        return content.split(`\n`);
+    }
+    catch (err) {
+        console.error(chalk.red(err));
+        return[];
+    }
+};
+
 module.exports = { 
     name: `--generate`, 
     async run(userIndex) { 
+        const sentences = await readContent(FILE_SENTENCES_PATH); 
+        const titles = await readContent(FILE_TITLES_PATH); 
+        const categories = await readContent(FILE_CATEGORIES_PATH);
         const [count] = userIndex; 
         if (count > MAX_COUNT) { 
             console.error(chalk.red(`Не больше ${MAX_COUNT} объявлений`)); 
             process.exit(ExitCode.error); 
         } 
         const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT; 
-        const content = JSON.stringify(generateOffers(countOffer));
+        const content = JSON.stringify(generateOffers(countOffer,titles,categories,sentences));
         try{
             await fs.writeFile(FILE_NAME, content);
             console.log(chalk.green(`File recorded!`)); 
--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const chalk = require(`chalk`);
+
 module.exports = {
     name: `--help`,
     run() {
@@ -14,5 +15,4 @@ module.exports = {
         --generate <count>    формирует файл mocks.json`;
         console.log(chalk.gray(text));
     }
-
 };
