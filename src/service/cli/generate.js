b5df66e682d0344efb8431280e17ca49fc846ee9
Liz_CHe <alishia@list.ru>
2020-02-24 12:24:45 +0300

    Fixed Promise.All()

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -12,72 +12,71 @@ const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const getOfferType = {
-    OFFER: `offer`,
-    SALE: `sale`,
+  OFFER: `offer`,
+  SALE: `sale`,
 };
 
 const getSumRestrict = {
-    MIN: 1000,
-    MAX: 100000,
+  MIN: 1000,
+  MAX: 100000,
 };
 
 const getPictureRestrict = {
-    MIN: 0,
-    MAX: 16,
+  MIN: 0,
+  MAX: 16,
 };
 
 const getPicFileName = (number) => `item${number < 10 ? `0${number}` : number}.jpg}`;
 
 const generateOffers = (count, titles, categories, sentences) => (
-    Array(count).fill({}).map(() => (
-        {
-            title: titles[getReandomInt(0, titles.length - 1)],
-            picture: getPicFileName(getReandomInt(getPictureRestrict.MIN, getPictureRestrict.MAX)),
-            description: shuffle(sentences).slice(1, 5).join(` `),
-            type: Object.keys(getOfferType)[Math.floor(Math.random() * Object.keys(getOfferType).length)],
-            sum: getReandomInt(getSumRestrict.MIN, getSumRestrict.MAX),
-            category: [categories[getReandomInt(0, categories.length - 1)]],
-        }))
+  Array(count).fill({}).map(() => (
+      {
+        title: titles[getReandomInt(0, titles.length - 1)],
+        picture: getPicFileName(getReandomInt(getPictureRestrict.MIN, getPictureRestrict.MAX)),
+        description: shuffle(sentences).slice(1, 5).join(` `),
+        type: Object.keys(getOfferType)[Math.floor(Math.random() * Object.keys(getOfferType).length)],
+        sum: getReandomInt(getSumRestrict.MIN, getSumRestrict.MAX),
+        category: [categories[getReandomInt(0, categories.length - 1)]],
+      }))
 );
 
 const readContent = async (filePath) => {
-    try {
-        const content = await fs.readFile(filePath, `utf8`);
-        return content.split(`\n`);
-    }
-    catch (err) {
-        console.error(chalk.red(err));
-        return [];
-    }
+  try {
+      const content = await fs.readFile(filePath, `utf8`);
+      return content.split(`\n`);
+  }
+  catch (err) {
+      console.error(chalk.red(err));
+      return [];
+  }
 };
 
 module.exports = {
-    name: `--generate`,
-    async run(userIndex) {
-        const sentences = await readContent(FILE_SENTENCES_PATH);
-        const titles = await readContent(FILE_TITLES_PATH);
-        const categories = await readContent(FILE_CATEGORIES_PATH);
+  name: `--generate`,
+  async run(userIndex) {
+      const [sentences, titles, categories] = await Promise.all([
+          readContent(FILE_SENTENCES_PATH),
+          readContent(FILE_TITLES_PATH),
+          readContent(FILE_CATEGORIES_PATH),
+      ]);
+      const [count] = userIndex;
 
-        Promise.all([sentences, titles, categories]);
+      if (count > MAX_COUNT) {
+          console.error(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
+          process.exit(ExitCode.error);
+      }
 
-        const [count] = userIndex;
+      const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+      const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
-        if (count > MAX_COUNT) {
-            console.error(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
-            process.exit(ExitCode.error);
-        }
-
-        const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-        const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
-
-        try {
-            await fs.writeFile(FILE_NAME, content);
-            console.log(chalk.green(`File recorded!`));
-            process.exit(ExitCode.success);
-        }
-        catch {
-            console.error(chalk.red(`Error writing file`));
-            process.exit(ExitCode.error);
-        }
-    }
+      try {
+          await fs.writeFile(FILE_NAME, content);
+          console.log(chalk.green(`File recorded!`));
+          process.exit(ExitCode.success);
+      }
+      catch {
+          console.error(chalk.red(`Error writing file`));
+          process.exit(ExitCode.error);
+      }
+  }
 };
