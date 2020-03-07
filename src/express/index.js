3a6af0601cb0f2e733f1f9e8947fa2034488731a
Liz_CHe <alishia@list.ru>
2020-03-07 10:45:59 +0300

    script added

--- src/express/index.js
+++ src/express/index.js
@@ -7,11 +7,6 @@ const router = require(`./routes/index`);
 const DEFAULT_PORT = 8080;
 const app = express();
 
-module.exports = {
-  name: `--router`,
-  run() {
-    app.use(router);
-    app.listen(DEFAULT_PORT,
-        () => console.log(chalk.green(`Server of start: ${DEFAULT_PORT}`)));
-  },
-};
+app.use(router);
+app.listen(DEFAULT_PORT,
+    () => console.log(chalk.green(`Server of start: ${DEFAULT_PORT}`)));
--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,14 +4,12 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
-const router = require(`../../express/index`);
 
 const Cli = {
   [generate.name]: generate,
   [help.name]: help,
   [version.name]: version,
   [server.name]: server,
-  [router.name]: router,
 };
 
 module.exports = {
